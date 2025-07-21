e48fe5242491c270a5f1d03eba41b21132b763ab
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-22 03:19:11 +0700

    Add @nestjs/terminus package and integrate HealthModule into AppModule for improved health check functionality in the NestJS application.

--- nest-tutorial/src/app.module.ts
+++ nest-tutorial/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { HealthModule } from './health/health.module';
 
 
 @Module({
-  imports: [PostModule,MongooseModule.forRoot('mongodb://root:example@localhost:27017/'), UserModule],
+  imports: [PostModule,MongooseModule.forRoot('mongodb://root:example@localhost:27017/'), UserModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
