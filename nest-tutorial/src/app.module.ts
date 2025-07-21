66510b2ef9c1d530b7b4f0271b4bb3b72cda7dd3
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-21 23:40:21 +0700

    Import UserModule into AppModule to integrate user management functionality in the NestJS application.

--- nest-tutorial/src/app.module.ts
+++ nest-tutorial/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [PostModule,MongooseModule.forRoot('mongodb://root:example@localhost:27017/')],
+  imports: [PostModule,MongooseModule.forRoot('mongodb://root:example@localhost:27017/'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
